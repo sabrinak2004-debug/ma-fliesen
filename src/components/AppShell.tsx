c472bb3171ded9ddf5ac895f815a60297c606ce1
„Sabrina <sabrinak2004gmail.com>
2026-03-04 22:10:22 +0100

    :wq

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -191,7 +191,7 @@ export default function AppShell({
                   priority
                   style={{ objectFit: "contain" }}
                 />
-                <div style={{ minWidth: 0 }}></div>
+                <div style={{ minWidth: 3 }}></div>
               </div>
               <div style={{ fontWeight: 900, color: "rgba(255,255,255,0.95)", lineHeight: 1.05 }}>
                 ma-fliesen
