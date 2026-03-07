329028519a7a78b2ba2fdd995a9c0c37c113f7a0
„Sabrina <sabrinak2004gmail.com>
2026-03-07 02:04:21 +0100

    :wq

--- src/app/admin/urlaubsantraege/page.tsx
+++ src/app/admin/urlaubsantraege/page.tsx
@@ -610,7 +610,7 @@ export default function UrlaubsantraegePage() {
             marginTop: 16,
           }}
         >
-          <div style={{ minWidth: 0, width: "100%", height: "70%" }}>
+          <div style={{ minWidth: 0, width: "100%", height: "80%" }}>
             <div className="label">Mitarbeiter</div>
             <select
               className="input"
@@ -618,7 +618,7 @@ export default function UrlaubsantraegePage() {
               onChange={(e) => setSelectedUserId(e.target.value)}
               style={{
                 width: "100%",
-                height: "70%",
+                height: "80%",
                 minWidth: 0,
                 boxSizing: "border-box",
                 display: "block",
