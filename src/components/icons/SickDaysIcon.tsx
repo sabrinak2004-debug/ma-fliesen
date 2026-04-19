f9b380f73d3c63ce1ddd23e64eb27ef94c2b66b9
„Sabrina <sabrinak2004gmail.com>
2026-04-19 23:57:59 +0200

    :wq

--- src/components/icons/SickDaysIcon.tsx
+++ src/components/icons/SickDaysIcon.tsx
@@ -51,7 +51,6 @@ export default function SickBedIcon({
         stroke={color}
         strokeWidth={strokeWidth}
         strokeLinecap="round"
-        strokeLinejoin="round"
       />
 
       {/* Rechte Rundung runter */}
