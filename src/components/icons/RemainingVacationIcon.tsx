306a5c75e4b2655fca3abf3a7fe71eec428a243a
„Sabrina <sabrinak2004gmail.com>
2026-04-20 23:16:03 +0200

    :wq

--- src/components/icons/RemainingVacationIcon.tsx
+++ src/components/icons/RemainingVacationIcon.tsx
@@ -17,7 +17,7 @@ export default function RemainingVacationIcon({
     <svg
       width={size}
       height={size}
-      viewBox="0 0 64 64"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
