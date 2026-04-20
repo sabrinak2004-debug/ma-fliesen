34be2ef5fd3d6497ca2e187898372436bf3fb440
„Sabrina <sabrinak2004gmail.com>
2026-04-20 23:00:53 +0200

    :wq

--- src/components/icons/SickDaysIcon.tsx
+++ src/components/icons/SickDaysIcon.tsx
@@ -17,7 +17,7 @@ export default function SickBedIcon({
     <svg
       width={size}
       height={size}
-      viewBox="0 0 24 24"
+      viewBox="0 0 24 64"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       style={{
