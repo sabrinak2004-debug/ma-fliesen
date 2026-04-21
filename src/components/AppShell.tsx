48c124e173c94e7697c39a2c7ad03c62949f8a6b
„Sabrina <sabrinak2004gmail.com>
2026-04-21 17:50:09 +0200

    :wq

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -752,7 +752,7 @@ export default function AppShell({
   {
     href: "/admin/password-reset",
     labelKey: "passwordReset",
-    icon: <FontAwesomeIcon icon={faLock} />,
+    icon: <LockIcon />,
   },
 ];
 
