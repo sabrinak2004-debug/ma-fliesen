c7124d047632a3275bb066e6155f9291a7f4b4c1
„Sabrina <sabrinak2004gmail.com>
2026-04-24 19:23:18 +0200

    :wq

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default async function RootLayout({
         />
         <meta
           name="apple-mobile-web-app-status-bar-style"
-          content="default"
+          content="black-translucent"
         />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Mitarbeiterportal" />
