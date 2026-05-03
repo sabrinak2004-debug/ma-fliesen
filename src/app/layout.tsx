94d921112a4568b48277044b34a42382b4be8500
„Sabrina <sabrinak2004gmail.com>
2026-05-03 15:35:10 +0200

    taskleiste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default async function RootLayout({
         />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Mitarbeiterportal" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       </head>
       <body>
         <div className="app-root-shell">
